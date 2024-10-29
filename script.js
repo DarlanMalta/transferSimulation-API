4b2641e160a2a795a3e5aec207c71fc6f5a21be0
Darlan Malta <darlanmalta55@gmail.com>
2024-10-29 19:17:48 -0300

    commit que comenta o código e da manutenção em algumas functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.querySelector("#btn");
-
+//FUNÇÃO QUE RENDERIZA OS DADOS DA API
 let renderDb = (dataApi) => {
   const newDiv = document.createElement("div");
   newDiv.id = "Transaction-" + dataApi.id;
@@ -7,23 +7,44 @@ let renderDb = (dataApi) => {
   newName.textContent = "Nome-" + dataApi.nome;
   const valueTransfer = document.createElement("h5");
   valueTransfer.textContent = "Valor da transferência-" + dataApi.valor;
-
   newDiv.append(newName, valueTransfer);
   document.querySelector("#content").appendChild(newDiv);
 };
-
+// FUNÇÃO DO MÉTODO GET QUE PEGA E FAZ A CONVERSÃO DA API PARA ARQUIVOS JSON
 async function getData() {
   const data = await fetch("http://localhost:3000/translations").then((res) =>
     res.json()
   );
   data.forEach(data);
 }
-
+//FUNÇÃO DO MÉTODO PUT QUE ATUALIZA A API/BD
+document.querySelector("#alterData").addEventListener("click", async (ev) => {
+  ev.preventDefault();
+  const idPut = document.querySelector("#put").value;
+  const url = `http://localhost:3000/translations/${idPut}`;
+  const newData = {
+    nome: document.querySelector("#newName").value,
+    valor: document.querySelector("#newValue").value,
+  };
+  const responsePut = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newData),
+  });
+  const responsePutJs = await responsePut.json();
+  renderDb(responsePutJs);
+  document.querySelector("#put").value = " ";
+  document.querySelector("#newName").value = " ";
+  document.querySelector("#newValue").value = "";
+});
+//FUNÇÃO POST QUE ATUALIZA OS DADOS DA API/BD
 button.addEventListener("click", async (ev) => {
   ev.preventDefault();
   const newTransaction = {
-    nome: document.querySelector("#newName").value,
-    valor: document.querySelector("#newValue").value,
+    nome: document.querySelector("#nome").value,
+    valor: document.querySelector("#valor").value,
   };
   const response = await fetch("http://localhost:3000/translations", {
     method: "POST",
@@ -33,13 +54,16 @@ button.addEventListener("click", async (ev) => {
     body: JSON.stringify(newTransaction),
   });
   const responseJs = await response.json();
-  renderTransaction(responseJs);
+  renderDb(responseJs);
+  document.querySelector("#nome").value = " ";
+  document.querySelector("#valor").value = " ";
 });
-
+// FUNÇÃO QUE IMPLEMENTA O MÉTODO DELET
 document.querySelector("#deletData").addEventListener("click", async (ev) => {
   const deletId = document.querySelector("#delet").value;
   const urlDelet = `http://localhost:3000/translations/${deletId}`;
   await fetch(urlDelet, {
     method: "DELETE",
   });
+  document.querySelector("#delet").value = " ";
 });
